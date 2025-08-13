4f8bfbe08b22ea728ea6c7ce4820e387e25d7c42
Luke Bangs <bangsluke@gmail.com>
2025-08-13 11:33:03 +0100

    Added Umami web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import umami from "@yeskunall/astro-umami";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 import { readFileSync } from 'fs';
@@ -34,7 +35,7 @@ export default defineConfig({
     filter: (page) =>
       !page.includes("/blog/tags") &&
       !page.includes("/blog/techs"),
-  }),],
+  }), umami({ id: "fad6adfb-2b8b-4868-a0a9-59d4fd860488" })],
 
   vite: {
     plugins: [
