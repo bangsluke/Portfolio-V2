7c22baa326f67832cb82aff491a65a4a30750ab7
Luke Bangs <bangsluke@gmail.com>
2026-01-16 08:43:16 +0000

    Working cucumber test

--- __tests__/e2e/steps/downloadCV.ts
+++ __tests__/e2e/steps/downloadCV.ts
@@ -1,18 +1,6 @@
-import { After, Before, Given, Then, When } from '@cucumber/cucumber';
-import { type Browser, chromium, expect, type Page } from '@playwright/test';
-
-let page: Page;
-let browser: Browser;
-
-Before(async () => {
-	browser = await chromium.launch({ headless: false });
-	const context = await browser.newContext();
-	page = await context.newPage();
-});
-
-After(async () => {
-	await browser.close();
-});
+import { Given, Then, When } from '@cucumber/cucumber';
+import { expect } from '@playwright/test';
+import { page } from '../utils/browserSetup.ts';
 
 Given('the user is on the {string} page', async (url: string) => {
 	await page.goto(url);
