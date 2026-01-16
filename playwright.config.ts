80688a2331b69f981ec3d4976125283db49d00a4
Luke Bangs <bangsluke@gmail.com>
2026-01-16 09:10:12 +0000

    Updated Playwright config to correctly locate the test results and added them to gitignore

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { outputFolder: '__tests__/e2e/playwright-report' }]],
+  /* Output directory for test artifacts */
+  outputDir: '__tests__/e2e/test-results',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
