3666e2d211440e711c1f9392f7ce9bd48cada265
Luke Bangs <bangsluke@gmail.com>
2026-01-16 07:22:57 +0000

    Initial new Playwright tests added for some homepage testing

---
+++ __tests__/e2e/utils/testHelpers.ts
@@ -0,0 +1,9 @@
+import { Page } from '@playwright/test';
+
+/**
+ * Wait for page to be fully loaded
+ */
+export async function waitForPageLoad(page: Page) {
+	// Wait for DOM - networkidle is unreliable with continuous requests (analytics, websockets, etc.)
+	await page.waitForLoadState('domcontentloaded', { timeout: 10000 });
+}
